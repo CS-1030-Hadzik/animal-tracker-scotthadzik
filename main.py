23a7bf18f0e95316a924bf5ffc5507f8b1f98131
Scott Hadzik <scotthadzik@weber.edu>
2026-03-24 18:31:29 -0600

    FIN: dog class init

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from animal import Animal
-
+from dog import Dog
 
 if __name__ == "__main__":
 
-    dog1 = Animal("Fido", "Canine")
-    dog2 = Animal("Nala", "Canine")
+    #object of the animal class instance of the animal class. Intanciation the class
+    animal1 = Animal("Gus", "Mouse")
+    dog2 = Dog("Nala", "Canine", "Medium")
 
-    print(dog1)
-    dog1.speak()
+    print(animal1)
+    animal1.speak()
 
     print(dog2)
     dog2.speak()
-
-    # TODO: Create an instance of the Animal class
-    # TODO: Print the Animal instance
-    # TODO: Call the method to make a generic animal sound
-
-    # TODO: Create an instance of the Dog class
-    # TODO: Print the Dog instance
-    # TODO: Call the method to make the dog-specific sound
 
     # TODO print out all the animals
